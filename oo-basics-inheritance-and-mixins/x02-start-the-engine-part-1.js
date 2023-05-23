34efd948338eb98a7761e8d39b8036054b93ed99
oscos <cortes.oscar@outlook.com>
2023-05-23 16:56:55 -0700

    fixed type.

--- oo-basics-inheritance-and-mixins/x02-start-the-engine-part-1.js
+++ oo-basics-inheritance-and-mixins/x02-start-the-engine-part-1.js
@@ -41,17 +41,13 @@ class Vehicle {
 class Truck extends Vehicle {
   constructor(year) {
     super(year);
+    this.startEngine();
   }
-}
 
-class Car extends Vehicle {
-  constructor(year) {
-    super(year);
+  startEngine() {
+    console.log("Ready to go!");
   }
 }
 
 let truck = new Truck(2003);
 console.log(truck.year); // 2003
-
-let car = new Car(2015);
-console.log(car.year); // 2015
